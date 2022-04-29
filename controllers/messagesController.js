300895774777c0a35eb0af4472db33b64152d79e
Aazib Chaudhry <aazibch47@gmail.com>
2022-04-29 14:58:28 +0500

    Refactor messagesController

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -4,56 +4,29 @@ const AppError = require('../utils/appError');
 const filterObject = require('../utils/filterObject');
 
 exports.createMessage = catchAsync(async (req, res, next) => {
-    // Check if conversation exists
-    let conversation = await Conversation.findById(
-        req.params.conversationId
-    ).populate('participants');
+    const conversation = await Conversation.find({
+        _id: req.params.conversationId,
+        participants: { $in: [req.user._id] }
+    });
 
-    if (
-        !conversation ||
-        !conversation.participants.some(
-            (el) => el._id.toString() === req.user._id.toString()
-        )
-    )
+    if (!conversation)
         return next(new AppError('Conversation not found.', 404));
 
     let filteredBody = filterObject(req.body, 'content');
 
-    filteredBody = {
-        ...filteredBody,
-        conversation: req.params.conversationId,
-        sender: req.user._id
-    };
+    filteredBody.sender = req.user._id;
 
     conversation.messages.push(filteredBody);
+    conversation.deletedBy = undefined;
 
-    if (conversation.deletedBy) conversation.deletedBy = undefined;
-
-    conversation.unreadBy = [
-        conversation.participants.find(
-            (user) => req.user._id.toString() !== user._id.toString()
-        )
-    ];
+    conversation.unreadBy = conversation.participants.find(
+        (user) => filteredBody.sender.toString() !== user._id.toString()
+    );
 
     await conversation.save();
 
-    conversation = conversation.toObject();
-
-    const sender = conversation.participants.find(
-        (user) => req.user._id.toString() === user._id.toString()
-    );
-
     res.status(200).json({
         status: 'success',
-        data: {
-            convoId: conversation._id,
-            ...conversation.messages[conversation.messages.length - 1],
-            recipient: conversation.participants.find(
-                (user) => req.user._id.toString() !== user._id.toString()
-            )._id,
-            sender: conversation.participants.find(
-                (user) => req.user._id.toString() === user._id.toString()
-            )
-        }
+        data: conversation.messages[conversation.messages.length - 1]
     });
 });
